21f0a11fd59a12e08884f2ec0ef88d1cad86c78d
Sindre Sorhus <sindresorhus@gmail.com>
2021-03-22 15:21:42 +0700

    Update repo_linter.sh

--- .github/workflows/repo_linter.sh
+++ .github/workflows/repo_linter.sh
@@ -3,24 +3,23 @@
 # by default github actions doesn't fetch the whole repo so we are getting the latest commit on main
 git fetch origin main:main --depth=1
 
-# find the repo in the git diff and then set it to an env variables
+# Find the repo in the git diff and then set it to an env variables.
 REPO_TO_LINT=$(
-    git diff main readme.md | 
-    # Look for changes (indicated by lines startingi with +)
-    grep ^+ | 
-    # Get the line that includes the readme
-    grep -Eo 'https.*#readme' | 
-    # Get just the url
-    sed 's/#readme//')
+	git diff main readme.md |
+	# Look for changes (indicated by lines startingi with +)
+	grep ^+ |
+	# Get the line that includes the readme
+	grep -Eo 'https.*#readme' |
+	# Get just the url
+	sed 's/#readme//')
 
-# If there's no repo found exit quietly
-if [ -z ${REPO_TO_LINT+x} ];
-then 
-    echo "No new link found in the format:  https://....#readme"
+# If there's no repo found, exit quietly.
+if [ -z ${REPO_TO_LINT+x} ]; then
+	echo "No new link found in the format:  https://....#readme"
 else
-    echo "Cloning $REPO_TO_LINT"
-    mkdir cloned
-    cd cloned
-    git clone --depth=1 "$REPO_TO_LINT" .
-    npx awesome-lint
+	echo "Cloning $REPO_TO_LINT"
+	mkdir cloned
+	cd cloned
+	git clone --depth=1 "$REPO_TO_LINT" .
+	npx awesome-lint
 fi
