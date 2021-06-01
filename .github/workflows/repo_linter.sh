fffb5ed5ec771aa36fe72adb3d80f1f0fbff6034
Ray Berger <RayBB@users.noreply.github.com>
2021-06-01 07:41:47 -1000

    print steps

--- .github/workflows/repo_linter.sh
+++ .github/workflows/repo_linter.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 # Find the repo in the git diff and then set it to an env variables.
+
+git diff main -- readme.md
+git diff main -- readme.md | grep ^+
+git diff main -- readme.md | grep ^+ | grep -Eo 'https.*#readme' |
+git diff main -- readme.md | grep ^+ | grep -Eo 'https.*#readme' | sed 's/#readme//'
+
 REPO_TO_LINT=$(
 	git diff main -- readme.md |
 	# Look for changes (indicated by lines starting with +).
@@ -15,8 +21,4 @@ if [ -z ${REPO_TO_LINT+x} ]; then
 	echo "No new link found in the format:  https://....#readme"
 else
 	echo "Cloning $REPO_TO_LINT"
-	mkdir cloned
-	cd cloned
-	git clone --depth=1 "$REPO_TO_LINT" .
-	npx awesome-lint
 fi
